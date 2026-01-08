029745c9722b97433b8aaca28a4b44f6e5875426
antoniocarvajal938 <carvajalwk@gmail.com>
2026-01-08 19:10:44 +0100

    Add LoginPage Page Object

---
+++ pages/LoginPage.ts
@@ -0,0 +1,39 @@
+import {Page, Locator} from '@playwright/test';
+
+export class LoginPage {
+    private readonly page: Page;
+    private readonly usernameInput: Locator;
+    private readonly passwordInput: Locator;
+    private readonly loginButton: Locator;
+    private readonly errorMessage: Locator;
+
+    constructor(page: Page) {
+        this.page = page;
+        this.usernameInput = page.locator('[data-test="username"]');
+        this.passwordInput = page.locator('[data-test="password"]');
+        this.loginButton = page.locator('[data-test="login-button"]');
+        this.errorMessage = page.locator('[data-test="error"]');
+    }
+
+    async navigate(): Promise<void> {
+        await this.page.goto('/');
+    }
+
+    async login(username: string, password:string): Promise<void> {
+        await this.usernameInput.fill(username);
+        await this.passwordInput.fill(password);
+        await this.loginButton.click();
+    }
+
+    async getErrorMessage(): Promise<string | null>  {
+        if (await this.errorMessage.isVisible()) {
+            return await this.errorMessage.textContent();
+        }
+        return null;
+    }
+
+    async isLoginButtonVisible(): Promise<boolean> {
+        return await this.loginButton.isVisible();
+    }
+
+}
